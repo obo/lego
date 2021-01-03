14711ef34bf148231b7633e66abbf1e76cb3717c
Ondrej Bojar <obo@cuni.cz>
2021-01-03 14:09:16 +0000

    minor

--- s3parator/start.sh
+++ s3parator/start.sh
@@ -5,7 +5,6 @@ echo "Motors:"
 cat /sys/class/tacho-motor/*/address | sed 's/^/  /'
 
 mots=$(cat /sys/class/tacho-motor/*/address | sort)
-echo "Got: '$mots'"
 if [ "$(echo $mots)" == "ev3-ports:outA ev3-ports:outD" ]; then
   ./manual-mover.py
 else
